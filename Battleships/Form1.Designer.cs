f3826b1e21795417b44cd1f720c98db112ef5674
Elias Persson <EliasPersson050324@gmail.com>
2023-08-23 19:43:20 +0200

    Add project files.

---
+++ Battleships/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Battleships
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Start/Reset";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += startOrReset;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox1.Location = new Point(420, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(10, 459);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(191, 416);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Player";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(621, 416);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Computer";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(857, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Battleships/Form1.cs
@@ -0,0 +1,344 @@
+using System.Drawing.Text;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace Battleships
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ClientSize = new Size(40 * 20 + 50, 40*10+40);
+
+        }
+        public partial class Tile : Button
+        {
+            private bool occupied = false;
+            private bool hit = false;
+            public bool Occupied   // property
+            {
+                get { return occupied; }   // get method
+                set { occupied = value; }  // set method
+            }
+            public bool Hit   // property
+            {
+                get { return hit; }   // get method
+                set { hit = value; }  // set method
+            }
+        }
+
+        int arenaSize = 10;
+        int tileSize = 40;
+        Tile[,] playerArena;
+        Tile[,] enemyArena;
+
+        string[] boats = { "hLong", "hMid", "hShort", "vLong", "vMid", "vShort", "T", "done" };
+        int bIndex;
+        private void startOrReset(object sender, EventArgs e)
+        {
+            bIndex = 0;
+
+            try { foreach (Tile item in playerArena) { Controls.Remove(item); } } catch { }
+            try { foreach (Tile item in enemyArena) { Controls.Remove(item); } } catch { }
+            playerArena = new Tile[arenaSize, arenaSize];
+            enemyArena = new Tile[arenaSize, arenaSize];
+
+            for (int x = 0; x < arenaSize; x++)
+            {
+                for (int y = 0; y < arenaSize; y++)
+                {
+                    playerArena[x, y] = new Tile()
+                    {
+                        Name = $"Tile{x}-{y}",
+                        Width = tileSize,
+                        Height = tileSize,
+                        Location = new Point(10 + tileSize * x, 10 + tileSize * y),
+                        BackColor = Color.LightSkyBlue,
+                        Occupied = false,
+                        Hit = false 
+
+
+
+                    };
+                    Controls.Add(playerArena[x, y]);
+                    playerArena[x, y].Click += new EventHandler(this.place);
+                    playerArena[x, y].MouseEnter += new EventHandler(this.preview);
+                    playerArena[x, y].MouseLeave += new EventHandler(this.clear);
+                }
+            }
+            for (int x = 0; x < arenaSize; x++)
+            {
+                for (int y = 0; y < arenaSize; y++)
+                {
+                    enemyArena[x, y] = new Tile()
+                    {
+                        Name = $"EnemyTile{x}-{y}",
+                        Width = tileSize,
+                        Height = tileSize,
+                        Location = new Point(440 + tileSize * x, 10 + tileSize * y),
+                        BackColor = Color.LightSkyBlue,
+                        Occupied = false,
+                        Hit = false
+
+                    };
+                    enemyArena[x, y].Click += new EventHandler(this.playerAttack);
+                    Controls.Add(enemyArena[x, y]);
+                }
+            }
+
+        }
+        private void place(object sender, EventArgs e)
+        {
+            string shape = boats[bIndex];
+            Tile btn = (Tile)sender;
+            int btnX, btnY;
+            string coords = btn.Name.Replace("Tile", "");
+            btnX = int.Parse(coords.Split("-")[0]);
+            btnY = int.Parse(coords.Split("-")[1]);
+
+            boardInteract(shape, btnX, btnY, Color.Gray, false, playerArena);
+            if (boats[bIndex] == "done")
+            {
+                bIndex = 0;
+                while (boats[bIndex] != "done")
+                {
+                    AutoPlace();
+                }
+            }
+        }
+        private void preview(object sender, EventArgs e)
+        {
+            string shape = boats[bIndex];
+            Tile btn = (Tile)sender;
+            int btnX, btnY;
+            string coords = btn.Name.Replace("Tile", "");
+            btnX = int.Parse(coords.Split("-")[0]);
+            btnY = int.Parse(coords.Split("-")[1]);
+
+            boardInteract(shape, btnX, btnY, Color.LightGray, true, playerArena);
+        }
+        private void boardInteract(string shape, int btnX, int btnY, Color color, bool preview, Tile[,] arena)
+        {
+            try
+            {
+                switch (shape)
+                {
+                    case "hLong":
+                        if (!arena[btnX - 1, btnY].Occupied && !arena[btnX, btnY].Occupied && !arena[btnX + 1, btnY].Occupied && !arena[btnX+2, btnY].Occupied)
+                        {
+                            arena[btnX - 1, btnY].BackColor = color;
+                            arena[btnX, btnY].BackColor = color;
+                            arena[btnX + 1, btnY].BackColor = color;
+                            arena[btnX + 2, btnY].BackColor = color;
+
+
+                            if (!preview)
+                            {
+                                arena[btnX - 1, btnY].Occupied = true;
+                                arena[btnX, btnY].Occupied = true;
+                                arena[btnX + 1, btnY].Occupied = true;
+                                arena[btnX + 2, btnY].Occupied = true;
+                                bIndex++;
+                            }
+                        }
+                        break;
+                    case "hMid":
+                        if (!arena[btnX - 1, btnY].Occupied && !arena[btnX, btnY].Occupied && !arena[btnX + 1, btnY].Occupied)
+                        {
+                            arena[btnX - 1, btnY].BackColor = color;
+                            arena[btnX, btnY].BackColor = color;
+                            arena[btnX + 1, btnY].BackColor = color;
+
+                            if (!preview)
+                            {
+                                arena[btnX - 1, btnY].Occupied = true;
+                                arena[btnX, btnY].Occupied = true;
+                                arena[btnX + 1, btnY].Occupied = true;
+                                bIndex++;
+                            }
+                        }
+                        break;
+
+                    case "hShort":
+                        if (!arena[btnX, btnY].Occupied && !arena[btnX + 1, btnY].Occupied)
+                        {
+                            arena[btnX, btnY].BackColor = color;
+                            arena[btnX + 1, btnY].BackColor = color;
+                            if (!preview)
+                            {
+                                arena[btnX, btnY].Occupied = true;
+                                arena[btnX + 1, btnY].Occupied = true;
+                                bIndex++;
+                            }
+                        }
+                        break;
+                    case "vLong":
+                        if (!arena[btnX, btnY - 1].Occupied && !arena[btnX, btnY].Occupied && !arena[btnX, btnY + 1].Occupied && !arena[btnX, btnY + 2].Occupied)
+                        {
+                            arena[btnX, btnY - 1].BackColor = color;
+                            arena[btnX, btnY].BackColor = color;
+                            arena[btnX, btnY + 1].BackColor = color;
+                            arena[btnX, btnY + 2].BackColor = color;
+                            if (!preview)
+                            {
+                                arena[btnX, btnY - 1].Occupied = true;
+                                arena[btnX, btnY].Occupied = true;
+                                arena[btnX, btnY + 1].Occupied = true;
+                                arena[btnX, btnY + 2].Occupied = true;
+                                bIndex++;
+                            }
+                        }
+                        break;
+                    case "vMid":
+                        if (!arena[btnX, btnY - 1].Occupied && !arena[btnX, btnY].Occupied && !arena[btnX, btnY + 1].Occupied)
+                        {
+                            arena[btnX, btnY - 1].BackColor = color;
+                            arena[btnX, btnY].BackColor = color;
+                            arena[btnX, btnY + 1].BackColor = color;
+                            if (!preview)
+                            {
+                                arena[btnX, btnY - 1].Occupied = true;
+                                arena[btnX, btnY].Occupied = true;
+                                arena[btnX, btnY + 1].Occupied = true;
+                                bIndex++;
+                            }
+                        }
+                        break;
+                    case "vShort":
+                        if (!arena[btnX, btnY].Occupied && !arena[btnX, btnY - 1].Occupied)
+                        {
+                            arena[btnX, btnY].BackColor = color;
+                            arena[btnX, btnY - 1].BackColor = color;
+                            if (!preview)
+                            {
+                                arena[btnX, btnY].Occupied = true;
+                                arena[btnX, btnY - 1].Occupied = true;
+                                bIndex++;
+                            }
+                        }
+                        break;
+                    case "T":
+                        if (!arena[btnX - 1, btnY].Occupied && !arena[btnX, btnY].Occupied && !arena[btnX + 1, btnY].Occupied && !arena[btnX, btnY + 1].Occupied)
+                        {
+                            arena[btnX - 1, btnY].BackColor = color;
+                            arena[btnX, btnY].BackColor = color;
+                            arena[btnX + 1, btnY].BackColor = color;
+                            arena[btnX, btnY + 1].BackColor = color;
+                            if (!preview)
+                            {
+                                arena[btnX - 1, btnY].Occupied = true;
+                                arena[btnX, btnY].Occupied = true;
+                                arena[btnX + 1, btnY].Occupied = true;
+                                arena[btnX, btnY + 1].Occupied = true;
+                                bIndex++;
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch { }
+        }
+        private void clear(object sender, EventArgs e)
+        {
+            foreach (Tile item in Controls.OfType<Tile>())
+            {
+                if (item.BackColor == Color.LightGray)
+                { item.BackColor = Color.LightSkyBlue; }
+            }
+        }
+        private bool gameOver()
+        {
+            bool isOver = true;
+            foreach (Tile item in playerArena)
+            {
+                if (!item.Hit && item.Occupied)
+                    isOver = false;
+            }
+            return isOver;
+
+        }
+        private bool gameWin()
+        {
+            bool isOver = true;
+            foreach (Tile item in enemyArena)
+            {
+                if (!item.Hit && item.Occupied)
+                    isOver = false;
+            }
+            return isOver;
+
+        }
+        private void enemyAttack()
+        {
+            Random rng = new Random();
+            int xAttack, yAttack;
+            xAttack = rng.Next(0, arenaSize);
+            yAttack = rng.Next(0, arenaSize);
+
+            if (boats[bIndex] == "done" && !gameOver())
+            {
+                if (playerArena[xAttack, yAttack].Occupied && !playerArena[xAttack, yAttack].Hit)
+                {
+                    playerArena[xAttack, yAttack].BackColor = Color.IndianRed;
+                    playerArena[xAttack, yAttack].Hit = true;
+                }
+                else if (playerArena[xAttack, yAttack].Hit)
+                {
+                    enemyAttack();
+                }
+                else { playerArena[xAttack, yAttack].BackColor = Color.WhiteSmoke; }
+            }
+            if (gameOver())
+            {
+                MessageBox.Show("You Lose :(");
+            }
+        }
+        private void playerAttack(object sender, EventArgs e)
+        {
+            Random rng = new Random();
+            Tile btn = (Tile)sender;
+            int btnX, btnY;
+            string coords = btn.Name.Replace("EnemyTile", "");
+            btnX = int.Parse(coords.Split("-")[0]);
+            btnY = int.Parse(coords.Split("-")[1]);
+
+            if (boats[bIndex] == "done" && !gameWin())
+            {
+                if (enemyArena[btnX, btnY].Occupied && !enemyArena[btnX, btnY].Hit)
+                {
+                    enemyArena[btnX, btnY].BackColor = Color.IndianRed;
+                    enemyArena[btnX, btnY].Hit = true;
+                    //MessageBox.Show("Hit");
+                    enemyAttack();
+                }
+                else
+                {
+                    enemyArena[btnX, btnY].BackColor = Color.WhiteSmoke;
+                    //MessageBox.Show("miss");
+                    enemyAttack();
+                }
+                foreach (Tile item in enemyArena)
+                    if (item.Hit)
+                        item.Click -= new EventHandler(this.playerAttack);
+            }
+            if (gameWin())
+            {
+                MessageBox.Show("You win!!!");
+            }
+
+        }
+        private void AutoPlace()
+        {
+            Random rng = new Random();
+            int btnX, btnY;
+            btnX = rng.Next(0, arenaSize);
+            btnY = rng.Next(0, arenaSize);
+            string shape = boats[bIndex];
+
+            boardInteract(shape, btnX, btnY, Color.LightSkyBlue, false, enemyArena);
+        }
+    }
+}
